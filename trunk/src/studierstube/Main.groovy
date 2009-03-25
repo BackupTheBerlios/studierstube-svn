89219f4b14c62885b76272ccec07c99a376e915f
twel <twel@ddc7da14-e665-0410-b5cf-d48cb9bf70f7>
2009-03-25 13:31:53 +0000

    main / startup classes

---
+++ trunk/src/studierstube/Main.groovy
@@ -0,0 +1,23 @@
+/**************************************************************
+* This file is part of the project 'Studierstube' at berliOS. *
+* Copyright (c) 2005-2009 Stefan Holzmüller (twelwan@gmx.de)  *
+* The software and its sources are available under the terms  *
+* of the (3-clause) BSD license. See LICENSE.txt for details. *
+**************************************************************/
+
+package studierstube
+
+public static class Main
+{
+	public static final String NAME = "Studierstube";
+	public static final String VERSION = "0.1";
+	public static final String COPYRIGHT = "Copyright (c) 2005-2009";
+	public static final String AUTHOR = "Stefan Holzmüller";
+	public static final String EMAIL = "twelwan@gmx.de";
+	
+	public static void identify()
+	{
+		println "$NAME $VERSION"
+		println "$COPYRIGHT $AUTHOR ($EMAIL)"
+	}
+}
