ff51d4e8d032b2042084c59bf0d6e1142b0da28c
twel <twel@ddc7da14-e665-0410-b5cf-d48cb9bf70f7>
2009-04-02 17:21:19 +0000

    better code

--- trunk/src/tests/Test.groovy
+++ trunk/src/tests/Test.groovy
@@ -3,16 +3,16 @@ package tests
 //def xdiml = new XmlSlurper().parseText(this.class.getResource('../xml/zauber.xml').text)
 def xdiml = new XmlSlurper().parse(new File(this.class.getResource('../xml/zauber.xml').toURI()))
 def zaubersprueche = xdiml.children().children()
-//zaubersprueche.children().each {println it.@ID}
-//zaubersprueche.children().each {println it.Probe}
-//zaubersprueche.children().each {println it.Komplexität}
-//zaubersprueche.children().Varianten.each { it.children().each { if (it == "Kosten") println it } }
-zaubersprueche.children().each {
+zaubersprueche.children().each { zauber ->
 	def merkmale = []
-	it.Merkmale.children().each { merkmale += "$it" }
+	zauber.Merkmale.children().each { merkmale.add(it) }
 	def varianten = []
-	it.Varianten.children().each { varianten += "$it" }
-	def z = new Zauber(id:it.@ID, komplexität:it.Komplexität, probe:it.Probe.toString().split("/"), merkmale:merkmale, varianten:varianten)
-	println z.getId()
+	zauber.Varianten.children().each { varianten.add(it) }
+	def z = new Zauber(
+			id:zauber.@ID,
+			komplexität:zauber.Komplexität,
+			probe:zauber.Probe.toString().split("/"),
+			merkmale:merkmale,
+			varianten:varianten)
+	println z
 }
-
