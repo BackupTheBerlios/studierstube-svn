63d07e60e41609b0ec7596b0817f654c35a33693
twel <twel@ddc7da14-e665-0410-b5cf-d48cb9bf70f7>
2009-04-02 13:41:04 +0000

    some testing how nice Groovy deals with XML

--- trunk/src/tests/Test.groovy
+++ trunk/src/tests/Test.groovy
@@ -1,6 +1,9 @@
-/**
- * 
- */
 package tests
 
-println 'Hi'
+//def xdiml = new XmlSlurper().parseText(this.class.getResource('../xml/zauber.xml').text)
+def xdiml = new XmlSlurper().parse(new File(this.class.getResource('../xml/zauber.xml').toURI()))
+def zaubersprueche = xdiml.children().children()
+//zaubersprueche.children().each {println it.@ID}
+//zaubersprueche.children().each {println it.Probe}
+//zaubersprueche.children().each {println it.Komplexität}
+zaubersprueche.children().Varianten.each { it.children().each { if (it == "Kosten") println it } }
